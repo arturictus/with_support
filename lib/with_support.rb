31490669dfe50c2c2cadaff5a575f440235f6a61
Artur Pañach <arturictus@gmail.com>
2016-10-30 23:25:32 +0100

    initial commit

---
+++ lib/with_support.rb
@@ -0,0 +1,5 @@
+require "with_support/version"
+
+module WithSupport
+  # Your code goes here...
+end
---
+++ spec/with_support_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe WithSupport do
+  it 'has a version number' do
+    expect(WithSupport::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
